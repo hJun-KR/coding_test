2ee2c79c08140aaec20b97f0a88a5206f546e921
hJun-KR <165541212+hJun-KR@users.noreply.github.com>
2024-04-07 20:29:25 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main ( String[] args ) {
+        Scanner sc = new Scanner(System.in);
+        long a, b, c;
+        a = sc.nextLong();
+        b = sc.nextLong();
+        c = sc.nextLong();
+        System.out.println(a + b + c);
+    }
+}
