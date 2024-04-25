00f9bc7ee0ff55824cc43ac199549fabbb5fc309
hJun-KR <165541212+hJun-KR@users.noreply.github.com>
2024-04-25 15:40:08 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main (void)
+{
+    long long a, b, c;
+    
+    scanf("%lld %lld %lld", &a, &b, &c);
+    printf("%lld", (a + b + c));
+    
+    return 0;
+}
