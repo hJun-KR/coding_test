502e4e49da03dca969e838c197840551c0d396fb
hJun-KR <165541212+hJun-KR@users.noreply.github.com>
2024-04-28 12:31:09 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char b  = a.charAt(0);
+
+        System.out.println((int)b);
+    }
+}
