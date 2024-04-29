ec9e32274996e4e0b785cb2307c71485d8e9bbe6
hJun-KR <165541212+hJun-KR@users.noreply.github.com>
2024-04-29 22:19:59 +0900

    [Bronze III] Title: 팩토리얼, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = 1;
+        for (int i = 1; i <= a; i++) {
+            b *= i;
+        }
+        System.out.println(b);
+    }
+}
+
