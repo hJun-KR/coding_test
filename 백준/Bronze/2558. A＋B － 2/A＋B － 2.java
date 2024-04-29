64585036f5a396310a1bf9b17b7beeba92e3eaa4
hJun-KR <165541212+hJun-KR@users.noreply.github.com>
2024-04-29 21:17:08 +0900

    [Bronze V] Title: A+B - 2, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte a = sc.nextByte();
+        byte b = sc.nextByte();
+        System.out.println(a+b);
+    }
+}
