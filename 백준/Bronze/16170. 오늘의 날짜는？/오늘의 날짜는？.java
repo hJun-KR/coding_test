c8420c5aeeece640ded5143cf066f23426107556
hJun-KR <165541212+hJun-KR@users.noreply.github.com>
2024-06-26 09:06:31 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 100 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("2024");
+        System.out.println("06");
+        System.out.println("26");
+    }
+}
