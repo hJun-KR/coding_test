8d8a2d232c75c5cf73b7a3ef99a44a230dbe1c1b
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-07-10 11:47:21 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 184 ms, Memory: 18264 KB -BaekjoonHub

--- 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -3,24 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[][] str = new String[3][3];
-        
-        for(int i = 0; i < str.length; i++) {
-            for (int j = 0; j < str[i].length; j++) {
-                if (i == 1 && j == 1) {
-                    str[i][j] = ":" + sc.next() + ":" ;
-                } else {
-                    str[i][j] = ":fan:";
-                }
-            }
-        }
-        
-        for(int i = 0; i < str.length; i++) {
-            for (int j = 0; j < str[i].length; j++) {
-                System.out.print(str[i][j]);
-            }
-            System.out.println();
-        }
+        System.out.print(":fan::fan::fan:\n" + 
+        ":fan::" +sc.next()+ "::fan:\n" + 
+        ":fan::fan::fan:");
         
     }
 }
