b0a1530d77b2fd63791b7649f744e124e6aff915
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-07-11 11:27:21 +0900

    [Bronze IV] Title: 헛간 청약, Time: 176 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/19698. 헛간 청약/헛간 청약.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int size = (b / d) * (c / d); 
+
+        System.out.println(Math.min(size, a));
+
+    }
+}
