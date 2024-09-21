f48228edda54f3f3b4d7059088e3d5f7555e36a2
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-09-22 00:21:08 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 164 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int count = 0;
+        
+		while(sc.hasNext()) {
+			count++;
+			String str = sc.nextLine();
+		}
+        
+		System.out.println(count);
+
+	}
+}
