ad0b61a946063e830738e5fcd23ff5a5500cc733
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-09-25 21:51:37 +0900

    [Bronze V] Title: 큰 수 (BIG), Time: 380 ms, Memory: 28084 KB -BaekjoonHub

---
+++ 백준/Bronze/14928. 큰 수 （BIG）/큰 수 （BIG）.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        
+        String input = sc.next();
+        
+        long temp = 0;
+        
+        for (int i = 0; i < input.length(); i++) {
+        	temp = (temp * 10 + (input.charAt(i) - '0')) % 20000303;
+        }
+        
+        System.out.println(temp);
+        
+    }
+}
