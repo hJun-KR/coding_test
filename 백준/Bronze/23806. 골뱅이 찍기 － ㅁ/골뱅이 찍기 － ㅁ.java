2f30435aa172ad4eb5282cb62aba96b151e6dd59
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-10-01 21:11:21 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 744 ms, Memory: 25872 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int N = n * 3;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print ("@@@@@");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print ("@");
+            }
+            for (int j = 0; j < n; j++) {
+                System.out.print ("   ");
+            }
+            for (int j = 0; j < n; j++) {
+                System.out.print ("@");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print ("@@@@@");
+            }
+            System.out.println();
+        }
+
+    }
+}
