92a4fc39291b5292e409f8a160aaa4fe5a6a1d62
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-10-05 17:34:58 +0900

    [Bronze V] Title: Serca, Time: 764 ms, Memory: 28120 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int K = sc.nextInt();
+        
+        for (int i = 0; i < K; i++) {
+            System.out.println (
+                    " @@@   @@@ \n" +
+                    "@   @ @   @\n" +
+                    "@    @    @\n" +
+                    "@         @\n" +
+                    " @       @ \n" +
+                    "  @     @  \n" +
+                    "   @   @   \n" +
+                    "    @ @    \n" +
+                    "     @     ");
+        }
+    }
+}
