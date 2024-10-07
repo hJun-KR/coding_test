bba085fd94dd693034414568e085c26f83ccfca8
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-10-07 20:23:41 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 296 ms, Memory: 22100 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int star = sc.nextInt();
+        
+        for (int i = 0; i < star; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = star-i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
