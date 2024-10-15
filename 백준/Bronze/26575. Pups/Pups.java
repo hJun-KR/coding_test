970990f1c491f23278eed820479bba0f4aa251f8
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-10-15 22:25:09 +0900

    [Bronze V] Title: Pups, Time: 184 ms, Memory: 18096 KB -BaekjoonHub

---
+++ 백준/Bronze/26575. Pups/Pups.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++){
+            double d = sc.nextDouble();
+            double f = sc.nextDouble();
+            double p = sc.nextDouble();
+
+            String s = String.format("%.2f",d*f*p);
+            System.out.print("$");
+            System.out.println(s);
+        }
+
+    }
+}
