0900ae80234eb4a67cc1bbe2c71e8cd8b9ba03a6
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-10-24 21:57:33 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 196 ms, Memory: 18416 KB -BaekjoonHub

---
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        if(a < b)
+            System.out.println("-1");
+        else{
+            int x = (a + b) / 2;
+            int y = (a - b) / 2;
+            if(x + y == a && x - y == b)
+                System.out.println(x+" "+y);
+            else
+                System.out.println("-1");
+        }
+    }
+}
