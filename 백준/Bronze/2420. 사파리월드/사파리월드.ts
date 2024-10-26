5ac3eef82ced53fc12b9ebe3b62ab23eada48152
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-10-26 21:16:32 +0900

    [Bronze V] Title: 사파리월드, Time: 108 ms, Memory: 9472 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.ts
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('', (input: string) => {
+    const [N, M] = input.split(' ').map(Number);
+
+    if (N < M) {
+        console.log(M - N);
+    } else {
+        console.log(N - M);
+    }
+
+    rl.close();
+});
