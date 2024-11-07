4d39e575d0f8819199317fa8eb244f3ed5527a2d
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-11-07 23:37:19 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    double a, b;
+    cin >> a >> b;
+ 
+    cout.precision(15);
+    cout << fixed;
+    cout << a/b;
+    
+    return 0;
+}
