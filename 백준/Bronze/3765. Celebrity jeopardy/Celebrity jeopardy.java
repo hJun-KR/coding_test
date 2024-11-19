feadfb53ca1a70fe04d53c8d3ff18243f9f99cbc
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-11-19 21:23:57 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 168 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str;
+		while(sc.hasNext()) {
+			str = sc.nextLine();
+			System.out.println(str);
+		}
+
+	}
+}
