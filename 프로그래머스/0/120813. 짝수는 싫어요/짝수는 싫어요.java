0cd5bf0932e369922bdf4e060c76fea240ca39ea
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2024-12-24 13:39:32 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] solution(int n) {
-        int length = (n % 2 == 0) ? (n / 2) : (n / 2 + 1);
-        System.out.print(length);
-        int[] answer = new int[length];
+        int[] answer = new int[(n / 2) + (n % 2)];
         for (int i = 1; i <= n; i++) {
             if (i % 2 == 1) {
                 answer[i/2] = i;
