df80d5bab73efa44bf786ecfff1a0333f142961d
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2025-03-12 08:27:08 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 168 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int money = sc.nextInt();
+        System.out.println(money / 5);
+    }
+}
