ac9591dcbee2aafa46a881efa041ef5d3c03a097
박홍준 <165541212+hJun-KR@users.noreply.github.com>
2025-06-05 15:07:18 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 168 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N1 = sc.nextInt();
+        int N2 = sc.nextInt();
+        
+        System.out.println(N1 * N2);
+    }
+}
